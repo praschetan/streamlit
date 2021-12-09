da95462a0eb9b39e33ed23834aea10a4eec4c351
Chetan Prasad <cprasad@adobe.com>
2021-12-09 13:11:20 -0700

    hello world

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 
 
-st.write (""" # My first app """
-st.write (""" Project Griffon is a new, innovative beta product from [Adobe Experience Cloud](https://www.adobe.com/experience-cloud.html) to help you inspect, proof, simulate, and validate how you collect data or serve experiences in your mobile app. We're building Project Griffon to do the heavy lifting of getting your app implementation right, while you focus on creating engaging experiences. """)
+st.title (""" # My first app """
+st.text (""" Project Griffon is a new, innovative beta product from [Adobe Experience Cloud](https://www.adobe.com/experience-cloud.html) to help you inspect, proof, simulate, and validate how you collect data or serve experiences in your mobile app. We're building Project Griffon to do the heavy lifting of getting your app implementation right, while you focus on creating engaging experiences. """)
 
