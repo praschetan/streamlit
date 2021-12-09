19e41c2ef6a7490001922c88e5324f2a58e0fab0
Chetan Prasad <cprasad@adobe.com>
2021-12-09 13:39:02 -0700

    hello world

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import streamlit as st
 
 
 st.title ("My first app")
-st.markdown ('Project Griffon is a new, innovative beta product from [Adobe Experience Cloud](https://www.adobe.com/experience-cloud.html) to help you inspect, proof, simulate, and validate how you collect data or serve experiences in your mobile app. We're building Project Griffon to do the heavy lifting of getting your app implementation right, while you focus on creating engaging experiences.')
+st.markdown (""" Project Griffon is a new, innovative beta product from [Adobe Experience Cloud](https://www.adobe.com/experience-cloud.html) to help you inspect, proof, simulate, and validate how you collect data or serve experiences in your mobile app. We're building Project Griffon to do the heavy lifting of getting your app implementation right, while you focus on creating engaging experiences.""")
 
