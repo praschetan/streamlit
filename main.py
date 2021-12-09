14a63c74e96627b1cbb0924177e89cd1436f5b9b
Chetan Prasad <cprasad@adobe.com>
2021-12-09 13:42:01 -0700

    hello world

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ import streamlit as st
 st.title ("My first app")
 st.markdown (""" Project Griffon is a new, innovative beta product from [Adobe Experience Cloud](https://www.adobe.com/experience-cloud.html) to help you inspect, proof, simulate, and validate how you collect data or serve experiences in your mobile app. We're building Project Griffon to do the heavy lifting of getting your app implementation right, while you focus on creating engaging experiences.""")
 
+st.markdown ("""
+	## What can Project Griffon do for you?
+
+### Quick Setup
+
+Get started in a hurry with few lines of code. For mobile apps, Project Griffon works with the [Adobe Experience Platform Mobile SDK](../../) to help you inspect, simulate, and validate app events, location signals, configuration parameters, SDK logs, device information, and more.
+
+### No-hassle Connection
+
+With Project Griffon, we've made connecting your app with our service simple and reliable. We've saved you the hassle of using network proxies, [MiTM](https://en.wikipedia.org/wiki/Man-in-the-middle_attack) and other network gymnastics - connecting your app to Project Griffon is made as easy as scanning a QR code. """)
+
